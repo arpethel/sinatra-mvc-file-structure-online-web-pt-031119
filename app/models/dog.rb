6b3e3d414215226ff46d460d5ef4da1b1f4c9eb9
Ryan Pethel <arpethel@gmail.com>
2019-06-11 19:32:48 +0000

    Automatically backed up by Learn

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -1,18 +1,18 @@
 class Dog
   attr_accessor :name, :breed, :age
 
-  dogs = []
+  @@dogs = []
 
   def initialize(name, breed, age)
     @name = name
     @breed = breed
     @age = age
     # binding.pry
-    dogs << self
+    @@dogs << self
   end
 
   def self.all
-    dogs
+    @@dogs
   end
 
 end
